0bc430cfa8d476320040c9da7ae4704fe1ad61d9
Chev <riskyrains@proton.me>
2025-04-14 16:55:19 -0700

    Get export dialog working & update CMakeLists.txt
    New required dependency: GimpUI
    
    Export doesn't actually work yet, just got the UI working.

--- src/file-vtf.cpp
+++ src/file-vtf.cpp
@@ -14,26 +14,20 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include <babl/babl.h>
-#include <cstddef>
-#include <gegl-buffer.h>
-#include <gegl-types.h>
-#include <gio/gio.h>
-#include <glib-object.h>
-#include <glib.h>
-#include <libgimp/gimp.h>
-#include <libgimp/gimpbatchprocedure.h>
-#include <libgimp/gimpfileprocedure.h>
-#include <libgimp/gimpimageprocedure.h>
-#include <libgimp/gimptypes.h>
-#include <libgimpbase/gimpbaseenums.h>
-#include <libgimpbase/gimpmetadata.h>
-#include <vector>
+#include "vtfpp/ImageFormats.h"
 #include "vtfpp/VTF.h"
+#include <libgimp/gimp.h>
+#include <libgimp/gimpui.h>
+
+// Attribution constants
+#define ATTRIBUTION_AUTHOR "Chev <riskyrains@proton.me>"
+#define ATTRIBUTION_COPYRIGHT "GPL-3.0"
+#define ATTRIBUTION_DATE "2025"
 
 // Procedures prefixed with 'plug-in-chev' to avoid procedure name conflicts, like another VTF loading plugin
 #define PROC_VTF_LOAD "plug-in-chev-file-vtf-load"
 #define PROC_VTF_EXPORT "plug-in-chev-file-vtf-export"
+#define PROC_VTF_BINARY "file-vtf"
 
 struct _GimpVtf {
     GimpPlugIn parent_instance;
@@ -57,9 +51,12 @@ static GimpImage *load_image(GFile *file, GError **error);
 static GimpValueArray *gimp_vtf_export(GimpProcedure *procedure,
     GimpRunMode run_mode,
     GimpImage *image,
-    GimpDrawable **drawables,
+    GFile *file,
+    GimpExportOptions *options,
+    GimpMetadata *metadata,
     GimpProcedureConfig *config,
     gpointer run_data);
+static gboolean export_dialog(GimpImage *image, GimpProcedure *procedure, GimpProcedureConfig *config);
 
 static void gimp_vtf_class_init(GimpVtfClass *gclass) {
     GimpPlugInClass *plug_in_class = GIMP_PLUG_IN_CLASS(gclass);
@@ -74,7 +71,9 @@ static void gimp_vtf_class_init(GimpVtfClass *gclass) {
 static void gimp_vtf_init(GimpVtf *gimp_vtf) {}
 
 static GList *gimp_vtf_query_procedures(GimpPlugIn *plugin) {
-    GList *list = g_list_append(NULL, g_strdup(PROC_VTF_LOAD));
+    GList *list = NULL;
+    
+    list = g_list_append(list, g_strdup(PROC_VTF_LOAD));
     list = g_list_append(list, g_strdup(PROC_VTF_EXPORT));
 
     return list;
@@ -88,7 +87,7 @@ static GimpProcedure *gimp_vtf_create_procedure(GimpPlugIn *plugin, const gchar 
             plugin, name, GIMP_PDB_PROC_TYPE_PLUGIN, gimp_vtf_load, NULL, NULL);
         // Only run when no image is open
         gimp_procedure_set_sensitivity_mask(procedure, GIMP_PROCEDURE_SENSITIVE_NO_IMAGE);
-        gimp_procedure_set_menu_label(procedure, "_VTF image");
+        gimp_procedure_set_menu_label(procedure, "VTF image");
         gimp_procedure_set_documentation(
             procedure,
             "Loads files in VTF file format",
@@ -97,16 +96,172 @@ static GimpProcedure *gimp_vtf_create_procedure(GimpPlugIn *plugin, const gchar 
         );
         gimp_procedure_set_attribution(
             procedure,
-            "Chev <riskyrains@proton.me>",
-            "GPL-3.0",
-            "2025"
+            ATTRIBUTION_AUTHOR,
+            ATTRIBUTION_COPYRIGHT,
+            ATTRIBUTION_DATE
         );
         gimp_file_procedure_set_mime_types(GIMP_FILE_PROCEDURE(procedure), "image/vtf");
         gimp_file_procedure_set_extensions(GIMP_FILE_PROCEDURE(procedure), "vtf");
         gimp_file_procedure_set_magics(GIMP_FILE_PROCEDURE(procedure), "0,string,VTF\000");
     } else if (g_strcmp0(name, PROC_VTF_EXPORT) == 0) {
-        procedure = gimp_image_procedure_new(
-            plugin, name, GIMP_PDB_PROC_TYPE_PLUGIN, gimp_vtf_export, NULL, NULL);
+        procedure = gimp_export_procedure_new(
+            plugin, name, GIMP_PDB_PROC_TYPE_PLUGIN, TRUE, gimp_vtf_export, NULL, NULL);
+        //gimp_procedure_set_sensitivity_mask(procedure, GIMP_PROCEDURE_SENSITIVE_ALWAYS);
+        gimp_procedure_set_image_types(procedure, "*");
+        gimp_procedure_set_menu_label(procedure, "VTF image");
+        gimp_procedure_set_documentation(
+            procedure,
+            "Exports files in VTF file format",
+            "This plug-in exports Valve Texture Format (VTF) files.",
+            NULL
+        );
+        gimp_procedure_set_attribution(
+            procedure,
+            ATTRIBUTION_AUTHOR,
+            ATTRIBUTION_COPYRIGHT,
+            ATTRIBUTION_DATE
+        );
+        gimp_file_procedure_set_format_name(GIMP_FILE_PROCEDURE(procedure), "VTF");
+        gimp_file_procedure_set_mime_types(GIMP_FILE_PROCEDURE(procedure), "image/vtf");
+        gimp_file_procedure_set_extensions(GIMP_FILE_PROCEDURE(procedure), "vtf");
+        gimp_export_procedure_set_capabilities(
+            GIMP_EXPORT_PROCEDURE(procedure),
+            (GimpExportCapabilities)(
+                GIMP_EXPORT_CAN_HANDLE_RGB
+                | GIMP_EXPORT_CAN_HANDLE_ALPHA
+                | GIMP_EXPORT_CAN_HANDLE_GRAY
+                | GIMP_EXPORT_CAN_HANDLE_INDEXED
+                | GIMP_EXPORT_CAN_HANDLE_LAYERS_AS_ANIMATION
+            ),
+            NULL, NULL, NULL
+        );
+        
+        //
+        // VTF export arguments
+        //
+
+        // TODO: If the current image was an imported VTF, copy its settings here.
+
+        // Version (7.0-7.6), default 7.4
+        // 7.4 is what vtfpp uses by default; it's also the last version that most Source games support,
+        //  causing breakage in a lot of games in 7.5 and beyond
+        //  Source: https://developer.valvesoftware.com/wiki/VTF_(Valve_Texture_Format)#Versions
+        GimpChoice *choice_version = gimp_choice_new_with_values(
+            "7_0", 0, "7.0", NULL,
+            "7_1", 1, "7.1", NULL,
+            "7_2", 2, "7.2", NULL,
+            "7_3", 3, "7.3", NULL,
+            "7_4", 4, "7.4", NULL,
+            "7_5", 5, "7.5", NULL,
+            "7_6", 6, "7.6", NULL,
+            NULL
+        );
+        gimp_procedure_add_choice_argument(
+            procedure,
+            "version",
+            "VTF version",
+            "VTF file version (7.0 to 7.6).\nRecommended: Use 7.4 for best compatibility.",
+            choice_version,
+            "7_4",
+            G_PARAM_READWRITE
+        );
+
+        // Image format (DXT5, RGBA8888, etc.)
+        // TODO: Indent these better (I'm lazy)
+        GimpChoice *choice_image_format = gimp_choice_new_with_values(
+            "RGBA8888",                     (int)vtfpp::ImageFormat::RGBA8888, "RGBA8888", NULL,
+            "ABGR8888",                     (int)vtfpp::ImageFormat::ABGR8888, "ABGR8888", NULL,
+            "RGB888",                       (int)vtfpp::ImageFormat::RGB888, "RGB888", NULL,
+            "BGR888",                       (int)vtfpp::ImageFormat::BGR888, "BGR888", NULL,
+            "RGB565",                       (int)vtfpp::ImageFormat::RGB565, "RGB565", NULL,
+            "I8",                           (int)vtfpp::ImageFormat::I8, "I8", NULL,
+            "IA88",                         (int)vtfpp::ImageFormat::IA88, "IA88", NULL,
+            "P8",                           (int)vtfpp::ImageFormat::P8, "P8", NULL,
+            "A8",                           (int)vtfpp::ImageFormat::A8, "A8", NULL,
+            "RGB888_BLUESCREEN",            (int)vtfpp::ImageFormat::RGB888_BLUESCREEN, "RGB888_BLUESCREEN", NULL,
+            "BGR888_BLUESCREEN",            (int)vtfpp::ImageFormat::BGR888_BLUESCREEN, "BGR888_BLUESCREEN", NULL,
+            "ARGB8888",                     (int)vtfpp::ImageFormat::ARGB8888, "ARGB8888", NULL,
+            "BGRA8888",                     (int)vtfpp::ImageFormat::BGRA8888, "BGRA8888", NULL,
+            "DXT1",                         (int)vtfpp::ImageFormat::DXT1, "DXT1", NULL,
+            "DXT3",                         (int)vtfpp::ImageFormat::DXT3, "DXT3", NULL,
+            "DXT5",                         (int)vtfpp::ImageFormat::DXT5, "DXT5", NULL,
+            "BGRX8888",                     (int)vtfpp::ImageFormat::BGRX8888, "BGRX8888", NULL,
+            "BGR565",                       (int)vtfpp::ImageFormat::BGR565, "BGR565", NULL,
+            "BGRX5551",                     (int)vtfpp::ImageFormat::BGRX5551, "BGRX5551", NULL,
+            "BGRA4444",                     (int)vtfpp::ImageFormat::BGRA4444, "BGRA4444", NULL,
+            "DXT1_ONE_BIT_ALPHA",           (int)vtfpp::ImageFormat::DXT1_ONE_BIT_ALPHA, "DXT1_ONE_BIT_ALPHA", NULL,
+            "BGRA5551",                     (int)vtfpp::ImageFormat::BGRA5551, "BGRA5551", NULL,
+            "UV88",                         (int)vtfpp::ImageFormat::UV88, "UV88", NULL,
+            "UVWQ8888",                     (int)vtfpp::ImageFormat::UVWQ8888, "UVWQ8888", NULL,
+            "RGBA16161616F",                (int)vtfpp::ImageFormat::RGBA16161616F, "RGBA16161616F", NULL,
+            "RGBA16161616",                 (int)vtfpp::ImageFormat::RGBA16161616, "RGBA16161616", NULL,
+            "UVLX8888",                     (int)vtfpp::ImageFormat::UVLX8888, "UVLX8888", NULL,
+            "R32F",                         (int)vtfpp::ImageFormat::R32F, "R32F", NULL,
+            "RGB323232F",                   (int)vtfpp::ImageFormat::RGB323232F, "RGB323232F", NULL,
+            "RGBA32323232F",                (int)vtfpp::ImageFormat::RGBA32323232F, "RGBA32323232F", NULL,
+
+            "RG1616F",                      (int)vtfpp::ImageFormat::RG1616F, "RG1616F", NULL,
+            "RG3232F",                      (int)vtfpp::ImageFormat::RG3232F, "RG3232F", NULL,
+            "RGBX8888",                     (int)vtfpp::ImageFormat::RGBX8888, "RGBX8888", NULL,
+            "EMPTY",                        (int)vtfpp::ImageFormat::EMPTY, "EMPTY", NULL,
+            "ATI2N",                        (int)vtfpp::ImageFormat::ATI2N, "ATI2N", NULL,
+            "ATI1N",                        (int)vtfpp::ImageFormat::ATI1N, "ATI1N", NULL,
+            "RGBA1010102",                  (int)vtfpp::ImageFormat::RGBA1010102, "RGBA1010102", NULL,
+            "BGRA1010102",                  (int)vtfpp::ImageFormat::BGRA1010102, "BGRA1010102", NULL,
+            "R16F",                         (int)vtfpp::ImageFormat::R16F, "R16F", NULL,
+
+            "CONSOLE_BGRX8888_LINEAR",      (int)vtfpp::ImageFormat::CONSOLE_BGRX8888_LINEAR, "CONSOLE_BGRX8888_LINEAR", NULL,
+            "CONSOLE_RGBA8888_LINEAR",      (int)vtfpp::ImageFormat::CONSOLE_RGBA8888_LINEAR, "CONSOLE_RGBA8888_LINEAR", NULL,
+            "CONSOLE_ABGR8888_LINEAR",      (int)vtfpp::ImageFormat::CONSOLE_ABGR8888_LINEAR, "CONSOLE_ABGR8888_LINEAR", NULL,
+            "CONSOLE_ARGB8888_LINEAR",      (int)vtfpp::ImageFormat::CONSOLE_ARGB8888_LINEAR, "CONSOLE_ARGB8888_LINEAR", NULL,
+            "CONSOLE_BGRA8888_LINEAR",      (int)vtfpp::ImageFormat::CONSOLE_BGRA8888_LINEAR, "CONSOLE_BGRA8888_LINEAR", NULL,
+            "CONSOLE_RGB888_LINEAR",        (int)vtfpp::ImageFormat::CONSOLE_RGB888_LINEAR, "CONSOLE_RGB888_LINEAR", NULL,
+            "CONSOLE_BGR888_LINEAR",        (int)vtfpp::ImageFormat::CONSOLE_BGR888_LINEAR, "CONSOLE_BGR888_LINEAR", NULL,
+            "CONSOLE_BGRX5551_LINEAR",      (int)vtfpp::ImageFormat::CONSOLE_BGRX5551_LINEAR, "CONSOLE_BGRX5551_LINEAR", NULL,
+            "CONSOLE_I8_LINEAR",            (int)vtfpp::ImageFormat::CONSOLE_I8_LINEAR, "CONSOLE_I8_LINEAR", NULL,
+            "CONSOLE_RGBA16161616_LINEAR",  (int)vtfpp::ImageFormat::CONSOLE_RGBA16161616_LINEAR, "CONSOLE_RGBA16161616_LINEAR", NULL,
+            "CONSOLE_BGRX8888_LE",          (int)vtfpp::ImageFormat::CONSOLE_BGRX8888_LE, "CONSOLE_BGRX8888_LE", NULL,
+            "CONSOLE_BGRA8888_LE",          (int)vtfpp::ImageFormat::CONSOLE_BGRA8888_LE, "CONSOLE_BGRA8888_LE", NULL,
+        
+            "R8",                           (int)vtfpp::ImageFormat::R8, "R8", NULL,
+            "BC7",                          (int)vtfpp::ImageFormat::BC7, "BC7", NULL,
+            "BC6H",                         (int)vtfpp::ImageFormat::BC6H, "BC6H", NULL,
+            NULL
+        );
+        gimp_procedure_add_choice_argument(
+            procedure,
+            "image_format",
+            "Image format",
+            "Image format to use.\nRecommended: DXT1 for regular textures without alpha, DXT5 for textures with alpha.",
+            choice_image_format,
+            // TODO: Change this selection based on whether or not the current image has alpha
+            "DXT1",
+            G_PARAM_READWRITE
+        );
+
+        // Type (Standard, Environment Map, Volumetric Texture)
+        GimpChoice *choice_image_type = gimp_choice_new_with_values(
+            "standard",     0, "Standard", NULL,
+            "envmap",       1, "Environment Map", NULL,
+            "volumetric",   2, "Volumetric Texture", NULL,
+            NULL
+        );
+        gimp_procedure_add_choice_argument(
+            procedure,
+            "image_type",
+            "Image type",
+            "Image type (Standard, Environment Map, or Volumetric Texture).\nRecommended: Standard, unless you're making skyboxes, then use Environment Map.",
+            choice_image_type,
+            "standard",
+            G_PARAM_READWRITE
+        );
+
+        gimp_export_procedure_set_support_exif(GIMP_EXPORT_PROCEDURE(procedure), false);
+        gimp_export_procedure_set_support_iptc(GIMP_EXPORT_PROCEDURE(procedure), false);
+        gimp_export_procedure_set_support_xmp(GIMP_EXPORT_PROCEDURE(procedure), false);
+        gimp_export_procedure_set_support_profile(GIMP_EXPORT_PROCEDURE(procedure), false);
+        gimp_export_procedure_set_support_thumbnail(GIMP_EXPORT_PROCEDURE(procedure), true);
+        gimp_export_procedure_set_support_comment(GIMP_EXPORT_PROCEDURE(procedure), false);
     }
 
     return procedure;
@@ -215,12 +370,67 @@ static GimpImage *load_image(GFile *file, GError **error) {
 static GimpValueArray *gimp_vtf_export(GimpProcedure *procedure,
                                        GimpRunMode run_mode,
                                        GimpImage *image,
-                                       GimpDrawable **drawables,
+                                       GFile *file,
+                                       GimpExportOptions *options,
+                                       GimpMetadata *metadata,
                                        GimpProcedureConfig *config,
                                        gpointer run_data) {
-    gimp_message("Hello World!!");
+    GimpExportReturn export_type = GIMP_EXPORT_IGNORE;
+
+    gegl_init(NULL, NULL);
+
+    GimpImage *orig_image = image;
+
+    export_type = gimp_export_options_get_image(options, &image);
+    GList *drawables = gimp_image_list_layers(image);
+    //gboolean alpha = gimp_drawable_has_alpha(drawables->data);
+
+    // https://gitlab.gnome.org/GNOME/gimp/-/blob/master/plug-ins/file-jpeg/jpeg.c#L448
+    switch (run_mode) {
+        case GIMP_RUN_NONINTERACTIVE:
+            g_object_set(config, "show-preview", FALSE, NULL);
+            break;
+
+        case GIMP_RUN_INTERACTIVE:
+        case GIMP_RUN_WITH_LAST_VALS:
+            {
+                gimp_ui_init(PROC_VTF_BINARY);
+
+                export_dialog(orig_image, procedure, config);
+            }    
+            break;
+    }
+
+    if (export_type == GIMP_EXPORT_EXPORT) {
+        gimp_image_delete(image);
+    }
+
+    g_list_free(drawables);
+
+    gimp_message("Ran successfully");
 
     return gimp_procedure_new_return_values(procedure, GIMP_PDB_SUCCESS, NULL);
+}
+
+static gboolean export_dialog(GimpImage *image, GimpProcedure *procedure, GimpProcedureConfig *config) {
+    GtkWidget *dialog = gimp_export_procedure_dialog_new(
+        GIMP_EXPORT_PROCEDURE(procedure),
+        config,
+        image
+    );
+
+    gimp_procedure_dialog_fill(
+        GIMP_PROCEDURE_DIALOG(dialog),
+        "image_type", "version", "image_format",
+        NULL
+    );
+    
+    gboolean run_successful = gimp_procedure_dialog_run(GIMP_PROCEDURE_DIALOG(dialog));
+
+    gtk_widget_destroy(dialog);
+    
+    return run_successful;
+    //return true;
 }
 
 GIMP_MAIN(GIMP_VTF_TYPE);
